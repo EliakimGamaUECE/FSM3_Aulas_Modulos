678f36880f0448c1f859cc814340d116d308f7e0
EliakimGama <eliakimgama@hotmail.com>
2025-05-24 23:05:30 -0300

    Aulas 01 e 02

---
+++ 02_MODULO_INTERMADIARIO/FSM1_AULA_01/scripts.js
@@ -0,0 +1,11 @@
+var idade = 34
+var primeiro_nome = "Eliakim "
+
+//forma01
+document.writeln(`Meu nome é ${primeiro_nome} e minha idade é ${idade} anos.`)
+
+//forma02
+alert(`Meu nome é ${primeiro_nome} e minha idade é ${idade} anos.`)
+
+//forma03
+console.log(`Meu nome é ${primeiro_nome} e minha idade é ${idade} anos.`)
---
+++ 02_MODULO_INTERMADIARIO/FSM1_AULA_02/scripts.js
@@ -0,0 +1,24 @@
+var num1 = 20
+var num2 = 15
+
+document.writeln(num1 >= num2 ? "É Maior": "É Menor")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //forma02
+// alert(`Meu nome é ${primeiro_nome} e minha idade é ${idade} anos.`)
+
+// //forma03
+// console.log(`Meu nome é ${primeiro_nome} e minha idade é ${idade} anos.`)
