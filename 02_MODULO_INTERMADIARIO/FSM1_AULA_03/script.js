0ea4fb1aaf5b41b50d651104ff52275468b26b1b
EliakimGama <eliakimgama@hotmail.com>
2025-06-01 22:03:42 -0300

    Aulas 03 e 04

---
+++ 02_MODULO_INTERMADIARIO/FSM1_AULA_03/script.js
@@ -0,0 +1,77 @@
+//DECLARAÇÃO DE VARIÁVEIS (var, let e const)
+
+// var variavel = 1
+// let letiavel = 2
+// const constante = 3
+
+// document.writeln(constante)
+
+//ESTRUTURA DE DECISÃO (if e else)
+
+// let nota = 6
+// let media = 6
+
+// if(nota > media){
+//     document.writeln(`Aluno aprovado!!! Sua nota foi ${nota}`)
+// }
+// else if(nota<media){
+//     document.writeln(`Reprovado!!! sua nota foi ${nota}`)
+// }
+// else{
+//     document.writeln(`Passou raspando!!! kkkkk sua nota foi exatamente ${nota}`)
+// }
+
+//ESTRUTURA DE REPETIÇÃO (for)
+
+
+// for(let i=1;i<=10;i++){
+//     document.writeln(`Imprimindo o número ${i}<br>`)
+// }
+
+//ESTRUTURA DE CONTROLE (while e do while)
+
+// let i = 10
+// while(i<10){
+//     document.writeln(`O número é ${i}<br>`)
+//     i++
+// }
+
+// let j = 10
+// do{
+//     document.writeln(`O número é ${j}<br>`)
+//     j++
+// }
+// while(j<10)
+
+
+//EXERCÍCIO DA SOMA DOS NÚMEROS ATÉ O NÚMERO 100
+
+//JAILSON
+// soma = 0
+
+// for(let i = 0; i <= 100; i++) {
+//     soma += i
+// }
+
+// document.writeln(soma)
+
+
+//DANILO
+// let soma = 0;
+// let numero = 1;
+
+// while (numero <= 100) {
+//     soma += numero; 
+//     numero++
+// }
+
+// document.writeln(soma)
+
+//PEDRO
+// let n=0
+// for(let i=1;i<=100;i++){
+//     n=n+i
+//     if(i==100){
+//         document.writeln(`soma igual a: ${n}<br>`)
+//     }
+// }
---
+++ 02_MODULO_INTERMADIARIO/FSM1_AULA_04/script.js
@@ -0,0 +1,34 @@
+//ARRAYS
+
+// var frutas = ["Maçã", "Banana", "Laranja", "Uva", "Abacaxi"]
+
+//     document.writeln( `${frutas[0]}<br>`)
+
+//by Jailson
+// let comercio = ['arroz', 'feijão', 'macarrão', 'carne', 'leite', 'manteiga', 'óleo', 'batata', 'tomate', 'alface']
+
+// for(let i = 0; i < comercio.length; i++) {
+//     document.writeln(comercio[i])
+// }
+
+//OBJETOS
+
+let pessoa =
+    [
+        {
+            nome: "Eliakim",
+            profissao: "dev",
+            idade: 34
+        },
+        {
+            nome: "Mateus",
+            profissao: "dev",
+            idade: 18
+        }
+    ]
+console.log(pessoa.idade)
+
+
+
+//ARRAY DE OBJETOS
+
